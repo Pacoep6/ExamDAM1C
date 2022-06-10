6ba953a48701518f9b8fef2cae1b8b5b9e3c15c1
Guille <guillermo.rodriguez.gonzalez@iesjulianmarias.es>
2022-06-10 17:19:03 +0200

    Respuesta al ejercicio 1

--- src/entornosrecu2/EntornosRecu2.java
+++ src/entornosrecu2/EntornosRecu2.java
@@ -6,9 +6,9 @@ package entornosrecu2;
 
 public class EntornosRecu2 {
 
-    private int numRamas; //rellena desde el ejercicio 1!!
-    private String ramaPrincipal; //rellena desde el ejercicio 1!!
-    private String ramaEvolucionada; //rellena desde el ejercicio 1!!
+    private int numRamas = 3;
+    private String ramaPrincipal = "main";
+    private String ramaEvolucionada = "ramaPaco";
     
     public static void main(String[] args) {
         
