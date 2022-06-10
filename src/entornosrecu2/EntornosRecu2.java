b5b596c5fef9ab695d4025342c27d4f6c868787b
csanca <carmen.sanchez.caballero@iesjulianmarias.es>
2022-06-10 17:39:52 +0200

    Realizar ejercicios hasta el 8, estoy en el punto 9

--- src/entornosrecu2/EntornosRecu2.java
+++ src/entornosrecu2/EntornosRecu2.java
@@ -3,17 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package entornosrecu2;
+/**
+ * 
+ * @author Carmen Sánchez Caballero
+ * @version v1.0
+ */
 
 public class EntornosRecu2 {
-
+/**
+ * Variable numRamas recoge el numero de ramas que tiene el proyecto
+ * variable ramaPrincipal recoge el valor de la rama por defecto del proyecto
+ * variable ramaEvolucionada recoge la rama del proyecto que está más completa
+ * variable explicacionEj5 recoge la explicación que se ha realizado en el ejercico 5
+ * variable explicacionEj6 recoge la explicacion que se ha realizado en el ejercicio 6
+ */
     private int numRamas=3; //rellena desde el ejercicio 1!!
     private String ramaPrincipal="main"; //rellena desde el ejercicio 1!!
     private String ramaEvolucionada="ramaPaco"; //rellena desde el ejercicio 1!!
-    
+    private String explicacionEJ5="Se elimina el fichero /dev/null y se añade /b/build.xml en la revisión más antigua y en la cuarta más antigua se añaden comentarios a las variables numRamas, ramaPrincipal y ramaEvoluciona";
+   private String explicacionEj6="En la línea 12 había este comentario:  //aquí sólo se han programado ejemplos. Puedes hacer los tuyos";
     public static void main(String[] args) {
         
         //aquí sólo hacemos ejemplos para ver que funciona bien la clase Matemáticas
-        
+        /**
+         * el atributo calc1 es una variable de la clase Matemáticas. De ella se llamarán a los distintos métodos de la 
+         * clase Matemáticas.
+         */
         Matematicas calc1 = new Matematicas(11);//ejemplo
         System.out.println(calc1.capicua()); //ejemplo
         System.out.println(calc1.multiplicar(3));
--- src/entornosrecu2/Matematicas.java
+++ src/entornosrecu2/Matematicas.java
@@ -3,12 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package entornosrecu2;
-
+/**
+ * 
+ * @author Carmen Sánchez Caballero
+ * @version v1.0
+ * @see entornosrecu2.EntornosRecu2#main(java.lang.String[]) 
+ */
 
 public class Matematicas {
-
+/**
+ * variable que recoge el valor del número de entrada o 1
+ */
     private int numero;
-
+/**
+ * 
+ * @param numIn  el es numero que insertas como parámetro de entrada
+ */
     public Matematicas(int numIn) {
         if ((numIn > 999) || (numIn < 1)) {
             this.numero = 1;
@@ -17,7 +27,10 @@ public class Matematicas {
             this.numero = numIn;
         }
     }
-
+    /**
+     * 
+     * @return devuelve true o false según si el número es capicua o no
+     */
     public boolean capicua() {
         if (this.numero < 10) {
             return true;
@@ -39,6 +52,25 @@ public class Matematicas {
 
     }
     
+    /**
+     * En vez de tener varios if anidados, he puesto todos los valores verdaderos en un if
+     * y cuando un número no sea capicúa vaya a la parte del else.
+     * 
+     * @param num este numero que tiene de entrada para ver si es capicua o no
+     * @return  El return devuelve el valor de si el parametro de entrada es capicua
+     */
+    public boolean chequearNum(int num){
+            if(num<10 || (num<100 && (num/100==num%10) || num<1000 && (num/100==num%10))){
+                return true;
+            }
+            else{
+                return false;
+            } 
+    }
+    /**
+     * 
+     * @return devuelve el numero factorial
+     */
     public double factorial() {
         double result = 1;
         for (int i = 1; i < this.numero; i++) {
@@ -46,15 +78,25 @@ public class Matematicas {
         }
         return result;
     }
-
+    /**
+     * 
+     * @param num2 parametro de entrada 
+     * @return devuelve el valor del numero que insertas o 1 según el valor de las condiciones
+     */
     public int multiplicar(int num2){
         return (this.numero * num2);
     }
-
+/**
+ * 
+ * @return recoge el numero que se ha insertado
+ */
     public int getNumero() {
         return numero;
     }
-
+/**
+ * setea el numero introducido al parametro numero
+ * @param numero numero que se inserta
+ */
     public void setNumero(int numero) {
         this.numero = numero;
     }
