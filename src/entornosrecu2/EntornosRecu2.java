5bd465d4d7f1642c63f8c84785af4f6b64c6b677
unknown <ronal.arrayaza.gonzales@iesjulianmarias.es>
2022-06-10 17:08:07 +0200

    Modificaciones y Java Doc

--- src/entornosrecu2/EntornosRecu2.java
+++ src/entornosrecu2/EntornosRecu2.java
@@ -3,14 +3,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package entornosrecu2;
-
+/**
+ * 
+ * @author Ronal Arrayaza Gonzales
+ * @version VExamen3
+ */
 public class EntornosRecu2{
-
+    /**
+     * Declaracion de Variables
+     */
+    /**
+     * Variable que define el numero de ramas
+     */
     private int numRamas=3; //rellena desde el ejercicio 1!!
+    /**
+     * Variable que define la rama Principal
+     */
     private String ramaPrincipal="main"; //rellena desde el ejercicio 1!!
+    /**
+     * Variable de la rama mas Evolucionada
+     */
     private String ramaEvolucionada="main"; //rellena desde el ejercicio 1!!
+    /**
+     * Variable que almacena la Respuesta al Ejercicio 05
+     */
     private String ExplicaciónEj5="Se añaden las variables numRamas, ramaPrincipal y ramaEvolucionada y se ha movido el comentario de sitio ";
-    
+    /**
+     * Variable que almacena la Respuesta al ejercicio 6
+     */
+    private String EJ6="System.out.println(calc1.multiplicar(3))";
+   
+    /**
+     * Metodo Principal y de Ejecucion
+     * @param args Parametros de entrada para la ejecucion del main
+     */
     public static void main(String[] args) {
         
         //aquí sólo hacemos ejemplos para ver que funciona bien la clase Matemáticas
--- src/entornosrecu2/Matematicas.java
+++ src/entornosrecu2/Matematicas.java
@@ -4,12 +4,29 @@
  */
 package entornosrecu2;
 
-
+/**
+ * 
+ * @author Ronal Arrayaza Gonzales
+ * @version VExamen3
+ * @see Vea Tambien la clase principal <a href="EntornosRecu2.html">Clase Principal</a>
+ */
 public class Matematicas {
-
+    /**
+     * Variable numero 
+     */
     private int numero;
-
+    /**
+     * Metodo Constructor con un parametro de entrada
+     * @param numIn 
+     */
     public Matematicas(int numIn) {
+        chequearNum(numIn);
+    }
+    /**
+     * Metodo chequearNum se crea con  refactor - Introduce  - Method desde el constructor
+     * @param numIn 
+     */
+    public void chequearNum(int numIn) {
         if ((numIn > 999) || (numIn < 1)) {
             this.numero = 1;
             System.out.println("Número inválido. Se establece 1");
@@ -17,7 +34,10 @@ public class Matematicas {
             this.numero = numIn;
         }
     }
-
+    /**
+     * Metedo que verifica si un numero es capicua
+     * @return devuelve falso o verdadero de acuerdo al numero enviado en el contructor
+     */
     public boolean capicua() {
         if (this.numero < 10) {
             return true;
@@ -38,7 +58,10 @@ public class Matematicas {
         }
 
     }
-    
+    /**
+     * Metodo que obtiene el factorial del numero 
+     * @return Devuelve la acumulacion multiplicada
+     */
     public double factorial() {
         double result = 1;
         for (int i = 1; i < this.numero; i++) {
@@ -46,15 +69,25 @@ public class Matematicas {
         }
         return result;
     }
-
+    /**
+     * Metodo que multiplica un numero introducido por parametro
+     * @param num2 numero para multiplica
+     * @return devuelve la mutiplicacion del numero almacenado en el objeto con el parametro
+     */
     public int multiplicar(int num2){
         return (this.numero * num2);
     }
-
+    /**
+     * 
+     * @return devuelve el numero 
+     */
     public int getNumero() {
         return numero;
     }
-
+    /**
+     * 
+     * @param numero numero que sea asigna al nuevo numero
+     */
     public void setNumero(int numero) {
         this.numero = numero;
     }
